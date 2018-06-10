582313225028a5dedb91a54b79ab6096f629d869
Holly Voigts <hollysvoigts@gmail.com>
2018-06-09 23:18:43 -0500

    added files for rendering images

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+// import './App.css';
+import HeroCard from "./components/HeroCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import heroes from "./heroes.json";
+import "./App.css";
+  
+  class App extends Component {
+    // Setting this.state.friends to the friends json array
+    state = {
+      heroes
+      // unselectedHeroes: heroes,
+      // topScore: 0,
+      // curScore: 0,
+    };
+  
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  
+    // Map over this.state.friends and render a FriendCard component for each friend object
+    render() {
+      return (
+        <Wrapper>
+          <Title>Clicky-Game</Title>
+          {this.state.heroes.map(hero => (
+            <HeroCard
+              // removeFriend={this.removeFriend}
+              // id={friend.id}
+              // key={friend.id}
+              name={hero.name}
+              image={hero.image}
+            />
+          ))}
+        </Wrapper>
+      );
+    }
   }
-}
-
+  
 export default App;
+  
+
